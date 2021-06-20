2b35e2aa4fffc3c975fd7c144785f2668c6600b3
Sveta <bogatovasvetlanau@gmail.com>
2021-06-20 11:24:57 +0300

    Tests are created

---
+++ Pages/MainPage.cs
@@ -0,0 +1,128 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pages
+{
+    public class MainPage
+    {
+        private static IWebDriver _driver;
+        public MainPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public List<IWebElement> GetDivElements()
+        {
+            List<IWebElement> DivElements = _driver.FindElements(By.TagName("div")).ToList();
+            return DivElements;
+        }
+
+        public List<IWebElement> GetDivH1Elements()
+        {
+            List<IWebElement> DivH1Elements = _driver.FindElements(By.XPath(".//div//h1")).ToList();
+            return DivH1Elements;
+        }
+
+        public List<IWebElement> GetNewsElements()
+        {
+            List<IWebElement> NewsElements = _driver.FindElements(By.XPath(".//div[@class='readmore_item']")).ToList();
+            return NewsElements;
+        }
+
+        public IWebElement GetSpanElement()
+        {
+            IWebElement CityElement = _driver.FindElement(By.XPath(".//div/a/span[@class='cities_name'][text()='Чернигов']"));         
+            return CityElement;
+        }
+
+        public List<string> GetNewsElementsTitles()
+        {
+            List<IWebElement> NewsElements = _driver.FindElements(By.XPath(".//div[@class='readmore_item']/div/a/div[starts-with(@style,'back')]")).ToList();
+            List<string> elementTitles= new List<string>();
+            foreach(IWebElement element in NewsElements)
+            {
+                elementTitles.Add(element.Text);
+            }
+            return elementTitles;
+        }
+
+        public IWebElement GetElementsWithKyivTitle()
+        {
+            IWebElement CityElement = _driver.FindElement(By.XPath(".//span[text()='Киев']"));
+            return CityElement;
+        }
+
+        public string GetElementAfterKyiv()
+        {
+            IWebElement CityElement = _driver.FindElement(By.XPath(".//div[2]/div[2]/div[8]/a/span[1]"));
+            return CityElement.Text;
+        }
+
+        public List<IWebElement> GetTopMenuLinks()
+        {
+            List<IWebElement> MenuElements = _driver.FindElements(By.XPath(".//li[@class='nav_item']/a[@class='link blue']")).ToList();
+            return MenuElements;
+        }
+
+        public IWebElement Get3DaysWeather()
+        {
+            IWebElement WeatherElement = _driver.FindElement(By.XPath(".//a[contains(@title,'3 дня')]"));
+            return WeatherElement;
+        }
+
+        public bool GetTodaySelection()
+        {
+            IWebElement TodayElement = _driver.FindElement(By.XPath(".//a[@title='Погода в Харькове']"));
+            if (TodayElement.Enabled)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<string> GetLowHighTemperature()
+        {
+            IWebElement TodayElement = _driver.FindElement(By.XPath(".//a[@title='Погода в Харькове']"));
+            TodayElement.Click();
+            List<IWebElement> ListElement = _driver.FindElements(By.XPath(".//div/div[@class='tab_wrap']/div[@class = 'tab-content']/div[contains(@class, 'tabtempline tabtemp')]/div/div/div/div/span[contains(@class, 'temperature_c')][1]")).ToList();
+            List<string> elementTitles = new List<string>();
+            foreach (IWebElement element in ListElement)
+            {
+                elementTitles.Add(element.Text);
+            }
+            return elementTitles;
+        }
+
+        public List<IWebElement> GetDivElementsCss()
+        {
+            List<IWebElement> DivElements = _driver.FindElements(By.CssSelector("div")).ToList();
+            return DivElements;
+        }
+
+        public List<IWebElement> GetDivH1ElementsCss()
+        {
+            List<IWebElement> DivH1Elements = _driver.FindElements(By.CssSelector("div h1")).ToList();
+            return DivH1Elements;
+        }
+
+        public List<IWebElement> GetNewsElementsCss()
+        {
+            List<IWebElement> NewsElements = _driver.FindElements(By.CssSelector(".readmore_item")).ToList();
+            return NewsElements;
+        }
+
+        public bool GetElementsWithKyivTitleCSS()
+        {
+            List<IWebElement> CityElements = _driver.FindElements(By.CssSelector("span[class^='cities']")).ToList();
+            foreach (IWebElement element in CityElements)
+            {
+                if (element.Text == "Киев")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Tests.UI/TestsUI.cs
@@ -0,0 +1,146 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Pages;
+
+namespace Tests.UI
+{
+    public class TestsUI
+    {
+        private IWebDriver _driver;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://www.gismeteo.ua/");
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+
+        [Test, Order(1)]
+        [Category("UI")]
+        public void UITestDiv()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(279, mainPage.GetDivElements().Count);
+        }
+
+        [Test, Order(2)]
+        [Category("UI")]
+        public void UITestDivH1()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(1, mainPage.GetDivH1Elements().Count);
+        }
+
+        [Test, Order(3)]
+        [Category("UI")]
+        public void GetNewsElements()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(4, mainPage.GetNewsElements().Count);
+        }
+
+        [Test, Order(4)]
+        [Category("UI")]
+        public void GetSpanElements()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual("Чернигов", mainPage.GetSpanElement().Text);
+        }
+
+        [Test, Order(5)]
+        [Category("UI")]
+        public void GetNewsElementsTitles()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(4, mainPage.GetNewsElementsTitles().Count);
+        }
+
+        [Test, Order(6)]
+        [Category("UI")]
+        public void GetElementsWithKyivTitle()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.IsTrue(mainPage.GetElementsWithKyivTitle().Text == "Киев");
+        }
+
+        [Test, Order(7)]
+        [Category("UI")]
+        public void GetElementAfterKyiv()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual("Кривой Рог", mainPage.GetElementAfterKyiv());
+        }
+
+        [Test, Order(8)]
+        [Category("UI")]
+        public void GetTopMenuLinks()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(4, mainPage.GetTopMenuLinks().Count);
+        }
+
+        [Test, Order(9)]
+        [Category("UI")]
+        public void Get3DaysWeather()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(mainPage.Get3DaysWeather().Text, "3 дня");
+        }
+
+        [Test, Order(10)]
+        [Category("UI")]
+        public void GetTodaySelection()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.IsTrue(mainPage.GetTodaySelection());
+        }
+
+        [Test, Order(15)]
+        [Category("UI")]
+        public void GetLowHighTemperature()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(2, mainPage.GetLowHighTemperature().Count);
+        }
+
+        [Test, Order(11)]
+        [Category("UI")]
+        public void UITestDivCss()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(279, mainPage.GetDivElementsCss().Count);
+        }
+
+        [Test, Order(12)]
+        [Category("UI")]
+        public void UITestDivH1Css()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(1, mainPage.GetDivH1ElementsCss().Count);
+        }
+
+        [Test, Order(13)]
+        [Category("UI")]
+        public void GetNewsElementsCss()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.AreEqual(4, mainPage.GetNewsElementsCss().Count);
+        }
+
+        [Test, Order(14)]
+        [Category("UI")]
+        public void GetElementsWithKyivTitleCss()
+        {
+            var mainPage = new MainPage(_driver);
+            Assert.IsTrue(mainPage.GetElementsWithKyivTitleCSS());
+        }
+    }
+}
